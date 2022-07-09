636116d4d0a4f003b45ce46f11cf79e722d6fa6e
Joshua <joshuachowishere@gmail.com>
2022-07-08 17:14:20 -1000

    Fixed forward and backward

--- src/main/java/frc/robot/commands/DefaultDrive.java
+++ src/main/java/frc/robot/commands/DefaultDrive.java
@@ -26,7 +26,7 @@ public class DefaultDrive extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    drivetrain.arcadeDrive(-joystick.getRawAxis(1), joystick.getRawAxis(0));
+    drivetrain.arcadeDrive(joystick.getRawAxis(1), -joystick.getRawAxis(0));
   }
 
   // Called once the command ends or is interrupted.
